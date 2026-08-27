809a59b9693cb6b128e0cddce830ff36ed6d8c94
이병길 <byunggil@ibyeong-gil-ui-MacBookPro.local>
2026-08-27 17:51:12 +0900

    docs: update notes

--- Algorithm/JavaCheatSheet.java
+++ Algorithm/JavaCheatSheet.java
@@ -1,7 +1,14 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
+
+class Pair {
+  int x, y;
+
+  Pair(int x, int y) {
+    this.x = x;
+    this.y = y;
+  }
+}
 
 public class JavaCheatSheet {
   public static void main(String[] args) throws Exception {
@@ -129,8 +136,166 @@ public class JavaCheatSheet {
 
     System.out.println("-----------------------------------------------");
 
+    // 배열 출력
+    System.out.println("# 배열 출력");
+
+    // 1) primitive 배열 (1차원)
+    System.out.println("1) primitive 배열 (1차원)");
+    int[] arr = new int[] { 2, 4, 3, 1, 5 };
+    System.out.println(Arrays.toString(arr)); // [2, 4, 3, 1, 5]
+    System.out.println();
+
+    // 2) primitive 배열 (2차원 이상)
+    System.out.println("2) primitive 배열 (2차원 이상)");
+    int[][] arr2 = new int[][] { { 2, 4 }, { 3, 1, 5 } };
+    System.out.println(Arrays.deepToString(arr2));
+    System.out.println();
+
+    // 3) Collection (1차원)
+    System.out.println("3) Collection (1차원)");
+    List<Integer> l = new ArrayList<>(List.of(3, 2, 4, 1, 5));
+
+    /*
+     * List.of(1, 2, 3, ...)은 수정이 불가능한 List 구현체(ImmutableCollections$ListN)
+     * 를 반환한다. 하지만 이를 위와 같이 ArrayList의 초기값으로 사용하면,
+     * 해당 원소들이 새로운 ArrayList에 복사되므로 수정 가능한 리스트를 생성할 수 있다.
+     */
+
+    l.add(6);
+    System.out.println(l);
+    l.set(2, 7);
+    System.out.println(l);
+    l.remove(0);
+    System.out.println(l);
+    System.out.println();
+
+    // 4) Collection (2차원 이상)
+    System.out.println("4) Collection (2차원 이상)");
+    List<List<Integer>> l2 = new ArrayList<>();
+
+    l2.add(new ArrayList<>(List.of(1, 2, 3)));
+    l2.add(new ArrayList<>(List.of(4, 5, 6)));
+    l2.add(new ArrayList<>(List.of(7, 8, 9)));
+
+    System.out.println(l2);
+    System.out.println();
+
+    System.out.println("-----------------------------------------------");
+
     // 정렬
     System.out.println("# 정렬");
 
+    // 1) primitive 배열 (내림차순 불가)
+    System.out.println("1) primitive 배열 (내림차순 불가)");
+
+    arr = new int[] { 2, 4, 3, 1, 5 };
+
+    Arrays.sort(arr);
+    System.out.println(Arrays.toString(arr));
+    System.out.println();
+
+    // 2) Collection
+    System.out.println("2) Collection");
+    l = new ArrayList<>(List.of(2, 4, 3, 1, 5));
+
+    l.sort((e1, e2) -> {
+      return Integer.compare(e2, e1); // 내림차순
+    });
+    System.out.println(l);
+    System.out.println();
+
+    // 3) Collection (Pair)
+    System.out.println("3) Collection (Pair)");
+    List<Pair> l3 = new ArrayList<>();
+    l3 = new ArrayList<>(
+        List.of(
+            new Pair(1, 2),
+            new Pair(4, 1),
+            new Pair(2, 6),
+            new Pair(2, 3)));
+
+    l3.sort((e1, e2) -> {
+      if (e2.x != e1.x) {
+        return Integer.compare(e2.x, e1.x);
+      }
+      return Integer.compare(e1.y, e2.y);
+    }); // 첫 번째 원소 - 내림차순, 두 번째 원소 - 오름차순
+
+    for (int i = 0; i < l3.size(); i++) {
+      System.out.println(l3.get(i).x + " " + l3.get(i).y);
+    }
+
+    System.out.println("-----------------------------------------------");
+
+    // 스택, 큐, 덱
+    System.out.println("# 스택, 큐, 덱");
+
+    // 1) 스택
+    Deque<Integer> stack = new ArrayDeque<>();
+
+    for (int i = 0; i < 10; i++) {
+      stack.offerFirst(i);
+    }
+
+    Iterator<Integer> it = stack.iterator();
+    for (int i = 0; i < stack.size(); i++) {
+      int cur_v = it.next();
+
+      if (cur_v == 5) {
+        it.remove();
+      }
+
+      System.out.print(cur_v + " ");
+    }
+    System.out.println();
+
+    System.out.println(stack.poll());
+    System.out.println(stack.peek());
+
+    // 2) 큐
+    Deque<Integer> queue = new ArrayDeque<>();
+
+    for (int i = 0; i < 10; i++) {
+      queue.offerLast(i);
+    }
+
+    it = queue.iterator();
+    for (int i = 0; i < queue.size(); i++) {
+      int cur_v = it.next();
+
+      if (cur_v == 5) {
+        it.remove();
+      }
+
+      System.out.print(cur_v + " ");
+    }
+    System.out.println();
+
+    System.out.println(queue.poll());
+    System.out.println(queue.peek());
+
+    // 3) 덱
+    Deque<Integer> deque = new ArrayDeque<>();
+
+    for (int i = 0; i < 10; i++) {
+      deque.offerFirst(i);
+    }
+    for (int i = 0; i < 10; i++) {
+      deque.offerLast(i);
+    }
+
+    it = deque.iterator();
+    int d_size = deque.size();
+    for (int i = 0; i < d_size; i++) {
+      int cur_v = it.next();
+
+      if (cur_v == 5) {
+        it.remove();
+        continue;
+      }
+
+      System.out.print(cur_v + " ");
+    }
+    System.out.println();
   }
 }
